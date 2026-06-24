7d45d345d391abe4c8b1ef4e9f0cdcf6c3e21eb3
mohannedabuhassira2 <mohannedabuhassira2@gmail.com>
2026-06-24 14:01:58 +0200

    Launch a simple coroutine with Global Scope

--- app/src/main/java/com/example/playaroundbasics/MainActivity.kt
+++ app/src/main/java/com/example/playaroundbasics/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playaroundbasics
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,8 +13,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.playaroundbasics.ui.theme.PlayAroundBasicsTheme
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,6 +33,20 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        // GlobalScope is bad but let's just do that for now
+        // Idea is that this coroutine still lives until the app is killed which
+        // isn't the best with Android lifecycle
+        GlobalScope.launch {
+            // Sleep here pauses the coroutine but not the thread
+            // So it'll suspend the coroutine until the work of this suspend function
+            // is done and then resume the coroutine
+            Log.d(TAG, "Hello from coroutine: ${Thread.currentThread().name}")
+            delay(1000L)
+            Log.d(TAG, "Hello from coroutine: ${Thread.currentThread().name}")
+        }
+
+        Log.d(TAG, "Hello from thread: ${Thread.currentThread().name}")
     }
 }
 
